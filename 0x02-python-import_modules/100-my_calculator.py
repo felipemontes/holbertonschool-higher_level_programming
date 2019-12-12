1c3cd4897445a47f94a400918282f7590dddf7cb
Felipe <felipe_londono1@hotmail.com>
2019-12-12 20:22:25 +0000

    [FIX]: fixed last conditional

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,20 +2,21 @@
 if __name__ == "__main__":
     import calculator_1 as calc
     import sys
-    var1 = int(sys.argv[1])
-    var2 = int(sys.argv[3])
-    oper = sys.argv[2]
-    if len(sys.argv) is not 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif oper == "+":
-        print("{} + {} = {}".format(var1, var2, calc.add(var1, var2)))
-    elif oper == "-":
-        print("{} - {} = {}".format(var1, var2, calc.sub(var1, var2)))
-    elif oper == "*":
-        print("{} * {} = {}".format(var1, var2, calc.mul(var1, var2)))
-    elif oper == "/":
-        print("{} / {} = {}".format(var1, var2, calc.div(var1, var2)))
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        var1 = int(sys.argv[1])
+        var2 = int(sys.argv[3])
+        oper = sys.argv[2]
+        if oper == "+":
+            print("{} + {} = {}".format(var1, var2, calc.add(var1, var2)))
+        elif oper == "-":
+            print("{} - {} = {}".format(var1, var2, calc.sub(var1, var2)))
+        elif oper == "*":
+            print("{} * {} = {}".format(var1, var2, calc.mul(var1, var2)))
+        elif oper == "/":
+            print("{} / {} = {}".format(var1, var2, calc.div(var1, var2)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
