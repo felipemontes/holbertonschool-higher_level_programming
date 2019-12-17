b9ad388d0c3835241572d240df75fc4f2714f1af
Felipe <felipe_londono1@hotmail.com>
2019-12-17 22:52:56 +0000

    [FIX]: typo fix

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python4
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
+    l = len(my_list)
+    if idx < 0 or idx >= l:
         return my_list
     del my_list[idx]
     return my_list
