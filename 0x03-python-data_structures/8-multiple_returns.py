4e70c2f8688a8700b9c91803a137567e91585624
Felipe <felipe_londono1@hotmail.com>
2019-12-17 21:18:35 +0000

    [FIX]: changed conditional

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        l = None
-    else:
-        l = len(sentence)
+    l = len(sentence)
     f = sentence[0]
-    res = (l, f)
+    if l == 0:
+        res(l, None)
+    else:
+        res = (l, f)
     return res
