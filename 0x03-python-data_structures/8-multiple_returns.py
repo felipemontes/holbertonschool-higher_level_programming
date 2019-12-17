e25b60ea1e5bd0cef369e81d4e47ba07517c2191
Felipe <felipe_londono1@hotmail.com>
2019-12-17 21:28:32 +0000

    [FIX]: added permissions

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     l = len(sentence)
-    f = sentence[0]
+
     if l == 0:
         res = (l, None)
     else:
-        res = (l, f)
+        res = (l, sentence[0])
     return res
