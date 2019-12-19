0b0c3a4b1f437954cd328cdd2697d99c4731a8ad
Felipe <felipe_londono1@hotmail.com>
2019-12-19 22:25:26 +0000

    [FIX]: added condition if the string is null

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    values={'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    if not roman_string:
+        return 0
+    values = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     numbers = []
     for char in roman_string:
         numbers.append(values[char])
