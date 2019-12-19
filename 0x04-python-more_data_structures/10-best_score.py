082b946744e197295e4e4d8908eb2dc29c982f4c
Felipe <felipe_londono1@hotmail.com>
2019-12-19 20:22:06 +0000

    [FIX]: corrected conditional

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        res = max(a_dictionary.keys())
+        res = max(a_dictionary, key=a_dictionary.get)
     return res
