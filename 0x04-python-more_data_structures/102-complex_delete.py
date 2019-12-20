70039e19ee71de62ea7ed7908e8c84807ce25a91
Felipe <felipe_londono1@hotmail.com>
2019-12-20 04:58:25 +0000

    [FIX]: deleted first conditional

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if not a_dictionary:
-        return a_dictionary
     for keys in a_dictionary.keys():
         if value == a_dictionary[keys]:
             a_dictionary.pop(keys)
