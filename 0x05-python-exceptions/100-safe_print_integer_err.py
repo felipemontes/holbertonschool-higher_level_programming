eebda1751230a1db9d857e3bf161c833a7c78cc9
Felipe <felipe_londono1@hotmail.com>
2020-01-08 20:19:35 +0000

    [FIX]: corrected sys call

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as ne:
-        print("Exception: {}".format(ne), file=sys.stderr)
+    except ValueError as ve:
+        sys.stderr.write("Exception: {}\n".format(ve))
         return False
