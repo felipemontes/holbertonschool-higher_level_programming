81a360cd03f3671edb1d574867dc40bbb10a09ee
Felipe <felipe_londono1@hotmail.com>
2020-01-08 18:34:59 +0000

    [FIX]: corrected exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 def safe_print_list(my_list=[], x=0):
 	cont = 0
-	try:
-		for elem in range(my_list[0], my_list[x]):
-			print("{}".format(elem), end='')
-			cont += 1
-	except IndexError:
-		for i in my_list:
-			print("{}".format(i), end='')
-			cont += 1
+	for i in range(x):
+                try:
+                        print("{}".format(my_list[i]), end='')
+                        cont += 1
+                except IndexError:
+                        break
 	print()
 	return cont
