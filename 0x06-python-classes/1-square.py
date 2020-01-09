71bfabec6fa195faca158780127e96f0b9ed78fe
Felipe <felipe_londono1@hotmail.com>
2020-01-09 17:54:46 +0000

    [ADD]: square with a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" creating a class square
+"""
+
+
+class Square:
+    def __init__(self, size):
+        """ Constructor method that ask for the size
+    Args:
+        size (int): size of the square
+        """
+        self.__size = size
