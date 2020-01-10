d3fe3dedd081ff84816ef444b92df1c653bed02e
Felipe <felipe_londono1@hotmail.com>
2020-01-10 21:03:56 +0000

    [ADD]: single linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+class Node:
+
+    def __init__(self, data, next_node=None):
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+
+    def __init__(self):
+        self.__head = None
+
+    def __str__(self):
+        strr = []
+        tmp = self.__head
+        while (tmp):
+            strr.append(str(tmp.data))
+            tmp = tmp.next_node
+        strr = sorted(strr)
+        return '\n'.join(strr)
+
+    def sorted_insert(self, value):
+        if self.__head is None:
+            newnode = Node(value)
+            newnode.data = value
+            newnode.next_node = self.__head
+            self.__head = newnode
+        else:
+            newnode = Node(value, None)
+            newnode.data = value
+            newnode.next_node = self.__head
+            self.__head = newnode
