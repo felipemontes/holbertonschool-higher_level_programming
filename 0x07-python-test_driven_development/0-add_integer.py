8266144ce6215d621ebddf58b157249c35f12d02
Felipe <felipe_londono1@hotmail.com>
2020-01-12 03:32:37 +0000

    [ADD]: function that adds two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """function that returns the sum of two ints"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)        
+    return a + b
