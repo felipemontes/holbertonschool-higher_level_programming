458bf5a614f3dbc38ee29e77d159b94f4ad95af8
Felipe <felipe_londono1@hotmail.com>
2020-01-14 03:23:24 +0000

    [ADD]: function to print a text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def text_indentation(text):
+    """ function to print a text """
     remp = ['?', '.', ':']
     new_str = ''
     if not isinstance(text, str):
         raise TypeError('text must be a string')
     for i in range(len(text)):
         if text[i] is ' ' and text[i - 1] in remp:
+            continue
+        print(text[i], end='')
+        if text[i] in remp:
             print('\n')
-        else:
-            print('{}'.format(text[i]), end='')
