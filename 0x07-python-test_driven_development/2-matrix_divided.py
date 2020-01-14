09684e000d0dadb0a8f47fa261e3126c77189e4a
Felipe <felipe_londono1@hotmail.com>
2020-01-14 03:20:54 +0000

    [ADD]: function to divide elem of a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
+    """ function to divide elements of a matrix """
     size = 0
     if div == 0:
         raise ZeroDivisionError('division by zero')
     for rows in matrix:
+        if not isinstance(rows, list):
+            raise TypeError('matrix must be a matrix (list of lists) '
+                            'of integers/floats')
         for elem in rows:
             size += 1
             if not isinstance(elem, (int, float)):
                 raise TypeError(
-                    'matrix must be a matrix (list of lists) of integers/floats')
+                    'matrix must be a matrix (list of lists) '
+                    'of integers/floats')
     if size % 2 != 0:
         raise TypeError(
             'Each row of the matrix must have the same size')
