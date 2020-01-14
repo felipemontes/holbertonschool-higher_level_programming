e0c8a74ecb879e984692ae6b13e8f725a1c7c8d7
Felipe <felipe_londono1@hotmail.com>
2020-01-14 04:53:28 +0000

    [ADD]: added new edge cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,6 +11,9 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([6, 3, 65, 77]), 77)
         self.assertEqual(max_integer(), None)
 
+    def check_eq(self):
+        self.assertEqual(max_integer([1, 1, 1]), 1)
+
     def negative_max(self):
         self.assertEqual(max_integer([-5, -3, -2, -1]), -1)
         self.assertEqual(max_integer([5, -88, -5, 6]), 6)
@@ -28,6 +31,8 @@ class TestMaxInteger(unittest.TestCase):
             max_integer([1, (2, 3)])
         with self.assertRaises(KeyError):
             max_integer({'key': 1, 'key2': 2})
+        with self.assertRaises(NameError):
+            max_integer(i)
 
 
 if __name__ == '__main__':
