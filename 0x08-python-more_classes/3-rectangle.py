0c1832fe986d38be359610af8511e853f1cdace1
Felipe <felipe_londono1@hotmail.com>
2020-01-15 02:19:59 +0000

    [ADD]: created area and perimeter

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+class Rectangle:
+    """ defining class rectangle """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        strr = ''
+        if self.__height is 0 or self.__width is 0:
+            return strr
+        for i in range(self.__height):
+            strr += '#' * self.__width + '\n'
+        strr = strr[:-1]
+        return strr
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        if self.__height is 0 or self.__width is 0:
+            return 0
+        else:
+            return 2 * (self.__height + self.__width)
