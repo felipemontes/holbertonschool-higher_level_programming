fc17c6c2bcc59bea03868bcbf202445e48f9791a
felipe <felipe_londono1@hotmail.com>
2020-01-20 22:22:55 +0000

    [FIX]: corrected name of the methods

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,25 +6,26 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def check_max(self):
+
+    def test_check_max(self):
         self.assertEqual(max_integer([1, 3]), 3)
         self.assertEqual(max_integer([6, 3, 65, 77]), 77)
         self.assertEqual(max_integer(), None)
 
-    def check_eq(self):
+    def test_check_eq(self):
         self.assertEqual(max_integer([1, 1, 1]), 1)
 
-    def negative_max(self):
+    def test_negative_max(self):
         self.assertEqual(max_integer([-5, -3, -2, -1]), -1)
         self.assertEqual(max_integer([5, -88, -5, 6]), 6)
 
-    def float_max(self):
+    def test_float_max(self):
         self.assertEqual(max_integer([3.2, 3.1, 3.0, 3.8]), 3.8)
 
-    def check_str(self):
+    def test_check_str(self):
         self.assertEqual(max_integer(['a', 'b', 'c']), 'c')
 
-    def error_out(self):
+    def test_error_out(self):
         with self.assertRaises(TypeError):
             max_integer(32)
         with self.assertRaises(TypeError):
