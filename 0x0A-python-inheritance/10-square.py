c8cc21701c3364157e982d2afe7ef3c0e664c3ad
Felipe <felipe_londono1@hotmail.com>
2020-01-20 16:57:52 +0000

    [ADD]: initialized super class constructor

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ new class that inhereits from rectangle"""
+    def __init__(self, size):
+        super().integer_validator('size', size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size * 2
