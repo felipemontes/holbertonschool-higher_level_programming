1e3d3769ff2dd40cc96a2d8f983dacbf6899bf7d
Felipe <felipe_londono1@hotmail.com>
2020-01-21 23:47:44 +0000

    [FIX]: added utf-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """function to read and print a file"""
-    with open(filename) as f:
+    with open(filename, "r", encoding="utf-8") as f:
         fil = f.read()
     print(fil)
