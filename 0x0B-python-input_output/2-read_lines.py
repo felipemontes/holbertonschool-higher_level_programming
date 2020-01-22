65d99338ca2d19915510ab6a72935f67f00782f7
Felipe <felipe_londono1@hotmail.com>
2020-01-22 00:05:29 +0000

    [ADD]: function to read n lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     """ function to read n lines"""
-    with open(filename) as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         if nb_lines <= 0:
             fl = f.read()
+            print(fl, end='')
         else:
-            fl = f.readline(nb_lines)
-    print(fl, end="")
+            lines = 0
+            for line in f:
+                print(line, end='')
+                lines += 1
+                if lines == nb_lines:
+                    break
