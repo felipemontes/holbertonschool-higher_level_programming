c6d5ad56f63ebae0af0e98aaabd784bcf4c9f41b
Felipe <felipe_londono1@hotmail.com>
2020-01-22 00:24:08 +0000

    [ADD]: function that returns tha atrrb that are passed

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Student:
+    """ class that defines a studen"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not isinstance(attrs, list):
+            return self.__dict__
+        for elem in attrs:
+            if not isinstance(elem, str):
+                return self.__dict__
+        newdic = {}
+        for string in attrs:
+            if string in self.__dict__.keys():
+                newdic[string] = self.__dict__[string]
+        return newdic
