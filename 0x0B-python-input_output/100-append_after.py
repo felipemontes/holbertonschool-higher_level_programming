d5b13a3518132cde389409a016657f7d0d2a93d4
Felipe <felipe_londono1@hotmail.com>
2020-01-22 03:25:07 +0000

    [ADD]: function to append after a given line

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def append_after(filename="", search_string="", new_string=""):
+    """ functio to append afert certain line"""
+    line = ''
+    with open(filename, 'r+') as f:
+        for words in f:
+            line += words
+        if search_string in words:
+            line += new_string
+    f.seek(0)
+    f.write(line)
