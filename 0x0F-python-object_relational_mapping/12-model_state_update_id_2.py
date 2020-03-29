8526ae78cd022b31c827ab6b10edf8bce0c311b2
Felipe <felipe_londono1@hotmail.com>
2020-03-29 20:22:45 +0000

    [Fix]: corrected syntax

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -2,20 +2,19 @@
 '''
 script that changes the name of a State object from the database
 '''
-from sqlalchemy import Column, Integer, String, create_engine
+import sys
+from sqlalchemy import (create_engine)
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-import sys
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker()
     Session.configure(bind=engine)
     session = Session()
-
     states = session.query(State).filter(State.id == 2).first()
     states.name = "New Mexico"
     session.commit()
