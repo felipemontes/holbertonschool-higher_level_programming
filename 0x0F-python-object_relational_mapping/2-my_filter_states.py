5f344f632c57a9a6401385f45d84085ccd25c71f
Felipe <felipe_londono1@hotmail.com>
2020-03-29 16:00:23 +0000

    [Add]: script that takes in an argument and displays all values

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+script that takes in an argument and displays all values in the states
+'''
+import MySQLdb
+import sys
+
+
+def mysqlconnect():
+
+    db_connection = None
+    db_connection = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                                    db=sys.argv[3], port=3306)
+    cursor = db_connection.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name='{}'".format(sys.argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db_connection.close()
+
+
+if __name__ == '__main__':
+    mysqlconnect()
