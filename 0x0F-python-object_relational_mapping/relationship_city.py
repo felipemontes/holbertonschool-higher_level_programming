b6bcbffef6621ae0d9a4e8d72229efcb7ee95a0a
Felipe <felipe_londono1@hotmail.com>
2020-03-29 21:50:44 +0000

    [Add]: fixed imports

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -8,12 +8,6 @@ from relationship_state import Base
 
 
 class City(Base):
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from relationship_state import Base
-
-
-class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
