2a8929404e40414e845d8e032f44adf8e509d8a9
Felipe <felipe_londono1@hotmail.com>
2020-04-09 19:18:49 +0000

    [Fix]: used recursion

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-""" peak search """
 
-
-def find_peak(ls):
-    if ls:
-        leng = len(ls)
-        peak = ls[0]
-        if ls[0] > ls[1]:
-            peak = ls[0]
-        for i in range(len(ls) - 1):
-            if ls[i] > ls[i - 1] and ls[i] > ls[i + 1]:
-                peak = ls[i]
-        return peak
-    else:
+def findPeakUtil(arr, low, high):
+    """ Find peak"""
+    if not arr:
         return None
+
+    mid = int((high + low)/2)
+
+    if ((mid == 0 or arr[mid - 1] <= arr[mid]) and (
+            mid == len(arr) - 1 or arr[mid + 1] <= arr[mid])):
+        return arr[mid]
+
+    elif (mid > 0 and arr[mid - 1] > arr[mid]):
+        return findPeakUtil(arr, low, (mid - 1))
+
+    else:
+        return findPeakUtil(arr, (mid + 1), high)
+
+
+def find_peak(list_of_integers):
+    """ find max and min"""
+    if not list_of_integers:
+        return None
+
+    Max = len(list_of_integers) - 1
+    Min = 0
+    return findPeakUtil(list_of_integers, Min, Max)
