f20cf393af759182953ae83af6feb192f21fd29c
Felipe <felipe_londono1@hotmail.com>
2020-04-15 19:37:04 +0000

    [Add]: corrected error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Script that handles error codes """
-from urllib import request, error
+import urllib.request
+import urllib.error
 import sys
 
 if __name__ == '__main__':
     try:
         with request.urlopen(sys.argv[1]) as response:
-            res = response.read()
-            print(res.decode('utf-8'))
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
             print('Error code: {}'.format(e.code))
