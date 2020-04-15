934d756a7ee0e2adf0b9e289dbe93900fefee0b5
Felipe <felipe_londono1@hotmail.com>
2020-04-15 19:38:53 +0000

    [Fix]: indentation error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
         with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-            print('Error code: {}'.format(e.code))
+        print('Error code: {}'.format(e.code))
