aaabd89d620f4ee2c343803c3af1ee8c44ad511e
Felipe <felipe_londono1@hotmail.com>
2020-05-05 19:28:37 +0000

    [Fix]: corrected output

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
-let array = [];
-let b = 0;
-
-if (process.argv.length === 2 || process.argv.length === 3) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  for (let i = 2; i < process.argv.length; i++) {
-    array[b] = parseInt(process.argv[i]);
-    b++;
-  }
-  array = array.sort();
-  array = array.reverse();
-  console.log(array[1]);
+  const list = process.argv.sort();
+  console.log(list.reverse()[1]);
 }
