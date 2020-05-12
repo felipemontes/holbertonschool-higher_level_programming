5f300c3ba696af7bb4793bcf3f738cd01ef7048d
Felipe <felipe_londono1@hotmail.com>
2020-05-12 23:00:27 +0000

    [Fix]: corrected logic

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
 
 request(process.argv[2], function (error, response, body) {
   if (error == null) {
-    const ids = [];
-    const res = JSON.parse(body);
-    var count = {};
-    for (let i = 0; i < res.length; i++) {
-      if (res[i].completed === true) {
-        ids.push(res[i].userId);
+    const resp = {};
+    const json = JSON.parse(body);
+    for (let i = 0; i < json.length; i++) {
+      if (json[i].completed === true) {
+        if (resp[json[i].userId] === undefined) {
+          resp[json[i].userId] = 0;
+        }
+        resp[json[i].userId]++;
       }
     }
-    ids.forEach(function (i) { count[i] = (count[i] || 0) + 1; });
-    console.log(count);
+    console.log(resp);
   }
 });
