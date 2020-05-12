af2fb17b2b7d76dec9ce6c71cd39f37f557e224a
Felipe <felipe_londono1@hotmail.com>
2020-05-12 02:53:29 +0000

    [Fix]: corrected console.log

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const list = require('./100-data').list;
-const list1 = [...list].map(function (num, index) {
+const list1 = list.map(function (num, index) {
   return num * index;
 });
 
-console.log(`${list}`);
-console.log(`${list1}`);
+console.log(list);
+console.log(list1);
