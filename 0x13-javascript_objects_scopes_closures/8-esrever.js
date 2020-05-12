13ca5fe69fc83e066d4361d5caf5674218ad9f1d
Felipe <felipe_londono1@hotmail.com>
2020-05-12 02:00:53 +0000

    [Add]: reversed list without built-in method

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+exports.esrever = function (list) {
+  const nlist = [];
+  let b = 0;
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    nlist[b] = list[i];
+    b++;
+  }
+
+  return nlist;
+};
